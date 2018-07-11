ae12f90f12c7f617702adf2600035ee9495278a9
ntshuxi <ntshuxekanizitha@gmail.com>
2018-07-11 14:02:39 +0200

    third commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WinnerPage } from '../winner/winner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+scoreTeamA=0;
+scoreTeamB=0;
+  score=0;
 
   constructor(public navCtrl: NavController) {
 
+   
+  
   }
-
+TeamA(value:number):void{
+  this.scoreTeamA+=value;
+}
+TeamB(value:number):void{
+  this.scoreTeamB+=value;
+}
+Reset():void{
+  this.scoreTeamA=0;
+  this.scoreTeamB=0;
+ }
+endMatch():void{
+this.navCtrl.push(WinnerPage)
+}
 }
